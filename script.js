fce575d6d10938b756af2ecf367e85dc9f78cc4a
Tanuj2123 <tanujk389@gmail.com>
2026-03-03 10:36:35 +0530

    Refactor dice game logic in script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var randomNumber2 = Math.floor((Math.random()*6) + 1);
 
 var randomDiceImage1 = "dice" + randomNumber1 + ".png"; 
 var randomDiceImage2 = "dice" + randomNumber2 + ".png"; 
-document.querySelectorAll("img")[0].setAttribute("src", "images/" + randomDiceImage1);
-document.querySelectorAll("img")[1].setAttribute("src", "images/" + randomDiceImage2);
+document.querySelectorAll("img")[0].setAttribute("src",randomDiceImage1);
+document.querySelectorAll("img")[1].setAttribute("src",randomDiceImage2);
 
 if(randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML = "Player 1 Wins! 🚩";
@@ -15,4 +15,5 @@ else if(randomNumber2 > randomNumber1){
 }
 else{
     document.querySelector("h1").innerHTML = "Draw!!";
+
 }
